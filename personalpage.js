25c459e7841660a1d2e961f2d5e9c7b7259cd6b7
Karo Mouradyan <karapetmouradyan@gmail.com>
2018-11-19 23:08:55 +0400

    c

--- personalpage.js
+++ personalpage.js
@@ -10,16 +10,17 @@ function smoothScroll(target,duration) {
 			startTime = currentTime;
 		}
 		let timeElapsed = currentTime - startTime;
-		let run = easeOutCubic(timeElapsed,startPosition,distance,duration);
+		let run = easeInOutQuad(timeElapsed,startPosition,distance,duration);
 		window.scrollTo(0,run);
 		if(timeElapsed < duration) {
 			requestAnimationFrame(animation);
 		}
 	}
-	function easeOutCubic(t, b, c, d) {
-		t /= d;
+	function  easeInOutQuad (t, b, c, d) {
+		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
 		t--;
-		return c*(t*t*t + 1) + b;
+		return -c/2 * (t*(t-2) - 1) + b;
 	};
 
 	requestAnimationFrame(animation);
@@ -35,26 +36,62 @@ sectionEduc.addEventListener('click', function() {
 let sectionAbout = document.querySelector('.sectionAb');
 
 sectionAbout.addEventListener('click', function() {
-	smoothScroll('.aboutme',1000);
+	smoothScroll('.aboutme',1200);
 });
 
 let sectionContact = document.querySelector('.sectionCon');
 
 sectionContact.addEventListener('click', function() {
-	smoothScroll('.contact',1000);
+	smoothScroll('.contact',1500);
 });
 
-
-window.onscroll = function() {scrollButton()};
-let x = document.querySelector(".scrollT");
-
-function scrollButton() {
-	x.style.opacity = "0.5";
-	x.addEventListener("click",function() {
-		smoothScroll("body",2000);
-	});
-}
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
+
+/*
+window.onscroll = function() {scrollButton()};
+let x = document.querySelector(".scrollT");
+let listenscroll = false;
+
+function scrollButton() {
+	if(pageYOffset === 0) {
+		x.style.opacity = "0"; 
+		listenscroll = false;
+	}
+	if(window.pageYOffset > 350) {
+		listenscroll = true;
+		x.style.opacity = "0.5"; 
+		if(listenscroll) {
+			x.addEventListener("click",function() {
+				listenscroll = false;
+				smoothScroll("body",1500);
+			});
+		}
+		
+	}
+}
+*/
+let x = document.querySelector(".scrollT");
+let listenscroll = false;
+
+window.onscroll = function() {
+	if(window.pageYOffset <= 300) {
+		x.style.opacity = "0"; 
+		listenscroll = false;
+		x.removeEventListener("click",listenScrollFunc);
+	}
+	if( !listenscroll && window.pageYOffset > 300 ) {
+		listenscroll = true;
+		x.style.opacity = "0.5";
+		x.addEventListener("click",listenScrollFunc);
+	}
+}
+
+function listenScrollFunc() {
+	listenscroll = false;
+	smoothScroll("body",1500);
+}
+
+
 
